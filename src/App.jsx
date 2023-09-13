008dc6ceb35416f0d9aed5d44f0e29986a41cb7c
elisaul batista <elisaulbatista@pursuit.org>
2023-09-12 22:07:13 -0400

    added music component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import "./tailwind.css";
 import { Button } from "reactstrap";
 
 import "./App.css";
+import StartScreen from "./Components/StartScreen";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -13,6 +14,7 @@ function App() {
     <>
       <div className="bg-blue-500 text-white p-4 font-bold underline">
         Hello, Tailwind!
+        <StartScreen/>
         <Button color="danger">Click Me</Button>
       </div>
     </>
--- src/Components/StartScreen.jsx
+++ src/Components/StartScreen.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
+import React from "react";
 
 export default function StartScreen() {
   return (
-    <div className='start-screen'>
+    <div className="start-screen">
       <h1>Hello Gamers, Welcome to Deezer Rhythm Run</h1>
       <p>
-      Deezer Rhythm Run is a music matching game where you must match discs to the rhythm of the music.
-        Get ready for a rhythm-filled challenge!
+        Deezer Rhythm Run is a music matching game where you must match discs to
+        the rhythm of the music. Get ready for this amazing rhythm-filled challenge.
       </p>
-      <div className='btn-container'>
-        <button className='start-btn'>Start Game</button>
+      <div className="btn-container">
+        <button className="start-btn">Start Game</button>
       </div>
     </div>
-  )
+  );
 }
