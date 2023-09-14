a66243a7270e9ea4b76faab28472320fbe01d396
Tonesha Rose <131703989+tonesharose31@users.noreply.github.com>
2023-09-13 23:00:14 -0400

    fetch function created

--- src/Components/MusicSelector.jsx
+++ src/Components/MusicSelector.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
 
-export default function MusicSelector() {
+import React, { useEffect, useState } from 'react';
+import { fetchDeezerTracks } from './api';
+
+const MusicSelector = () => {
+  const [tracks, setTracks] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchDeezerTracks();
+        setTracks(data.data);
+      } catch (error) {
+        console.error('Error fetching Deezer tracks:', error.message);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div>
-      
+      <h2>Select a Music Track</h2>
+      <ul>
+        {tracks.map((track) => (
+          <li key={track.id}>{track.title}</li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
+
+export default MusicSelector;
